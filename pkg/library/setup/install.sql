ee375931d4f90dfa361e6bd5787413b40c1b7a7e
Hank Shen <swh@admpub.com>
2021-12-15 09:49:09 +0800

    update

--- pkg/library/setup/install.sql
+++ pkg/library/setup/install.sql
@@ -172,4 +172,4 @@ CREATE TABLE `nging_collector_rule` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-14 13:21:42
+-- Dump completed on 2021-12-14 13:32:54
