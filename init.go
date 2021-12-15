44b37fae698a5d6db45e0ff8a81c4f679c97e63f
Hank Shen <swh@admpub.com>
2021-12-15 13:52:14 +0800

    update

--- init.go
+++ init.go
@@ -1,8 +1,11 @@
 package collector
 
 import (
+	"github.com/nging-plugins/collector/pkg/handler"
 	_ "github.com/nging-plugins/collector/pkg/library/setup"
 )
+
+var LeftNavigate = handler.LeftNavigate
 
 func init() {
 
